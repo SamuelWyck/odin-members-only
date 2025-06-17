4d4c63c84e5df6f3bff5bbf56bec8ba8be4c4e2c
samuel <samawyck@gmail.com>
2025-06-17 10:34:46 -0500

    Create basic app and hook up passport

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+require("dotenv").config();
+const path = require("node:path");
+const pool = require("./db/pool.js");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const passport = require("./passport.js");
+
+
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    store: new pgSession({
+        pool: pool,
+        tableName: "user_session"
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get("/", async function(req, res) {
+    return res.send("hello world");
+});
+
+
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, function() {console.log(`Server running on port ${PORT}!`)});
