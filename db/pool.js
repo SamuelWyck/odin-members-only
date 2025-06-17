37c79bae806d7c9f98b1fed9265dd2d30d0c9aaf
samuel <samawyck@gmail.com>
2025-06-17 10:01:40 -0500

    Create pool connection

---
+++ db/pool.js
@@ -0,0 +1,6 @@
+const {Pool} = require("pg");
+
+
+module.exports = new Pool({
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_URL}/${process.env.DB}`
+});
