cf897e7be2c424d17135c34c7041e4f40938e748
samuel <samawyck@gmail.com>
2025-06-23 12:10:22 -0500

    Hook up new message post route

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/querys.js");
+const {messageValidator} = require("../utils/validators.js");
+const {validationResult} = require("express-validator");
 
 
 
@@ -19,7 +22,31 @@ const logOutGet = asyncHandler(async function(req, res, next) {
 
 
 
+
+const newMessagePost = asyncHandler(async function(req, res) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).render("homePage", {
+            shown: true,
+            title: req.body.title,
+            text: req.body.text,
+            errors: errors.array()
+        })
+    }
+
+    const messageId = await db.addMessage(req.body.title, req.body.text);
+    await db.addMessageUserLink(req.user.id, messageId);
+    return res.redirect("/");
+});
+
+
+
 module.exports = {
     homeGet,
-    logOutGet
+    logOutGet,
+    newMessagePost: [
+        messageValidator,
+        newMessagePost
+    ]
 };
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -8,6 +8,7 @@ const homeRoute = Router();
 
 homeRoute.get("/", homeController.homeGet);
 homeRoute.get("/logout", loggedIn, homeController.logOutGet);
+homeRoute.post("/new", homeController.newMessagePost);
 
 
 module.exports = homeRoute;
